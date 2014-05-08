e8b545ae162ffd3f789baa27c0a4321068794739
Jeffrey <jeffrey@Chris-McFarlands-MacBook-Pro-15.local>
2014-05-08 09:06:08 -0400

    #1: add least-common-ancestor.py to consume input.py file

---
+++ least-common-ancestor.py
@@ -0,0 +1,10 @@
+'''
+least-common-ancestor.py
+
+Jeffrey Levesque
+05/08/2014
+
+Receives input data from input.py in order to generate a binary tree.  Then,
+prompts users for two nodes in order to determine a least common ancestor.
+'''
+
