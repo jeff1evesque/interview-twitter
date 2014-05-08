a393954778de49251e5d698155a2f5e930b0e357
Jeffrey <jeffrey@Chris-McFarlands-MacBook-Pro-15.local>
2014-05-08 10:57:48 -0400

    #1: least-common-ancestor.py consumes yet ignores certain cases

--- least-common-ancestor.py
+++ least-common-ancestor.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 '''
 least-common-ancestor.py
 
@@ -11,9 +12,15 @@ prompts users for two nodes in order to determine a least common ancestor.
 # location of input file
 filename = 'input.py'
 
+# comment tuple (and new-line) from input file
+ignore_type = ("#", "//", "\n")
+
 # read data from input.py file
-with open(filename) as f:
-  print(f.readlines())
+with open(filename, 'r') as f:
+  for line in f:
+    if any(line.startswith(s) for s in ignore_type):
+      continue
+    print(line)
 
 if __name__ == '__main__':
   print('hi')
