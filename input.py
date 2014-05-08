5f36ff9eef558761bbf85068fc74eaebb57b3917
Jeffrey <jeffrey@Chris-McFarlands-MacBook-Pro-15.local>
2014-05-08 10:55:27 -0400

    #1: remove block comment, and later add to README.md file

--- input.py
+++ input.py
@@ -1,38 +1,4 @@
-# input.py file is used to generate the binary tree 
-
-'''
-The following commands
-
-root = Node(3)
-
-tree = BinarySearchTree(root)
-
-tree.add(55)
-tree.add(2)
-tree.add(3)
-tree.add(30)
-tree.add(100)
-tree.add(45)
-tree.add(1)
-tree.add(43)
-tree.add(48)
-tree.add(25)
-tree.add(21)
-
-generates a tree structure as follows
-
-                 3
-                / \
-               /   \
-              /     \
-             2      55
-            / \    /  \
-           1   3  30   100
-                 /  \
-               25   45
-               /   /  \
-             21   43   48
-'''
+# input.py file is used to generate the binary tree, refer to the readme.md file
 
 root = Node(7)
 
