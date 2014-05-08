098153c274bb2bfc7050f71347872beec4159020
Jeffrey <jeffrey@Chris-McFarlands-MacBook-Pro-15.local>
2014-05-08 09:53:58 -0400

    #1: least-common-ancestor.py reads input.py file

--- least-common-ancestor.py
+++ least-common-ancestor.py
@@ -8,3 +8,12 @@ Receives input data from input.py in order to generate a binary tree.  Then,
 prompts users for two nodes in order to determine a least common ancestor.
 '''
 
+# location of input file
+filename = 'input.py'
+
+# read data from input.py file
+with open(filename) as f:
+  print(f.readlines())
+
+if __name__ == '__main__':
+  print('hi')
