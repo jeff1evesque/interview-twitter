f2ed344436e417566b1b745dd48a711dfa684614
Jeffrey <jeffrey@Chris-McFarlands-MacBook-Pro-15.local>
2014-05-09 11:02:58 -0400

    #6: correctly indent lca_algorithm() method (class_definitions.py)

--- class_definitions.py
+++ class_definitions.py
@@ -53,22 +53,22 @@ class Node:
     if self.right:
       self.right.print_tree_breadth()
 
-    def lca_algorithm(self, value1, value2):
-      node = root
-      ancestor = node
+  def lca_algorithm(self, value1, value2):
+    node = root
+    ancestor = node
 
-      while node != None:
-        if value1 < node.data and value2 > node.data:
-          ancestor = node
-          break
-        elif value1 == node.data or value2 == node.data:
-          break
-        elif value1 < node.data and value2 < node.data:
-          ancestor = node
-          node = node.left
-        elif value1 > node.data and value2 > node.data:
-          ancestor = node
-          node = node.right
+    while node != None:
+      if value1 < node.data and value2 > node.data:
+        ancestor = node
+        break
+      elif value1 == node.data or value2 == node.data:
+        break
+      elif value1 < node.data and value2 < node.data:
+        ancestor = node
+        node = node.left
+      elif value1 > node.data and value2 > node.data:
+        ancestor = node
+        node = node.right
 
-        # root node is empty
-        return ancestor
+      # root node is empty
+      return ancestor
